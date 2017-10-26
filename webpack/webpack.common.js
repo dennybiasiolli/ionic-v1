4495a4f703566d9db39b74992467fdf6ad64a52d
Denny Biasiolli <denny.biasiolli@gmail.com>
2017-10-26 11:45:02 +0200

    adding and configuring webpack sass loaders

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -11,9 +11,38 @@ module.exports = {
     'js/ionic-angular': [].concat(
       glob.sync('./js/angular/**/*.js'),
     ),
+    'css/ionic': './scss/ionic.scss',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '..', 'dist'),
   },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      }]
+    }, {
+      test: /\.(woff(2)?|eot|ttf|otf)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      }]
+    }]
+  }
 };
